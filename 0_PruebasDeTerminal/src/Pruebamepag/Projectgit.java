ea69f4642b28b261804963bf9bfb653868518613
rgxkokimoto <109067549+rgxkokimoto@users.noreply.github.com>
2024-02-12 10:49:01 +0100

    Update Projectgit.java Remoto

--- 0_PruebasDeTerminal/src/Pruebamepag/Projectgit.java
+++ 0_PruebasDeTerminal/src/Pruebamepag/Projectgit.java
@@ -3,10 +3,8 @@ package Pruebamepag;
 public class Projectgit {
 
 	public static void main(String[] args) {
-		
 		System.out.println("PruebasEgit");
 		System.out.println("Union rama recursiva");
-
 	}
 
 }
